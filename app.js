90ca29563bbe946f9491a4ca180493ae934dcb13
SevenSecrets <freddieshughes@googlemail.com>
2021-01-08 15:52:52 +0000

    example static template serving example text

--- app.js
+++ app.js
@@ -3,3 +3,12 @@ const logger = require('morgan');
 const app = express();
 const port = 3000;
 
+app.set('view engine', 'pug')
+
+app.get('/', (req, res) => {
+	res.render('index', { title: "article title", article: { title: "article title", body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." }});
+});
+
+app.listen(port, () => {
+	console.log(`Listening at port ${port}`);
+});

6187a6696dc541d3eaf6a6f64500d9a2e01bc404
SevenSecrets <freddieshughes@googlemail.com>
2021-01-12 15:05:58 +0000

    implementing http testing

---
+++ tests/routes.test.js
@@ -0,0 +1,18 @@
+const { doesNotMatch } = require('assert');
+const assert = require('assert');
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const expect = chai.expect;
+const app = require('../app');
+
+chai.use(chaiHttp);
+
+describe('get request', () => {
+	it('returns 200 OK for /', () =>{
+		chai.request(app)
+			.get('/')
+			.end((err, res) => {
+				expect(res).to.have.status(200);
+			});
+	});
+});

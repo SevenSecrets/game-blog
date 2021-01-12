90430abcfb6721f275445c8064124067f15caa7d
SevenSecrets <freddieshughes@googlemail.com>
2021-01-12 15:18:10 +0000

    correcting test - adding done

--- tests/routes.test.js
+++ tests/routes.test.js
@@ -8,11 +8,12 @@ const app = require('../app');
 chai.use(chaiHttp);
 
 describe('get request', () => {
-	it('returns 200 OK for /', () =>{
+	it('returns 200 OK for /', (done) =>{
 		chai.request(app)
 			.get('/')
 			.end((err, res) => {
 				expect(res).to.have.status(200);
+				done();
 			});
 	});
 });

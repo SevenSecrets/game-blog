0a66c1994a0c355e7acdfec9b96fda80b6e5c300
SevenSecrets <freddieshughes@googlemail.com>
2021-01-28 12:51:43 +0000

    adding /articles route

--- tests/routes.test.js
+++ tests/routes.test.js
@@ -1,11 +1,18 @@
 const request = require('supertest');
 const app = require('../app');
 
-describe("index requests", () => {
-	describe("get requests", () => {
+describe("get requests", () => {
+	describe("index route", () => {
 		it("responds 200", (done) => {
-			request(app).get('/')
+			request(app).get("/")
 				.expect(200, done);
 		})
 	});
+
+	describe("articles route", () => {
+		it("responds 200", (done) => {
+			request(app).get("/articles")
+				.expect(200, done);
+		})
+	})
 });

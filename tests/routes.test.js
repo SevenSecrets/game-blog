aec6874182227825b816d894d3c163b9fbb15413
SevenSecrets <freddieshughes@googlemail.com>
2021-02-04 13:29:40 +0000

    adding individual article page test

--- tests/routes.test.js
+++ tests/routes.test.js
@@ -1,18 +1,28 @@
 const request = require('supertest');
 const app = require('../app');
 
-describe("get requests", () => {
-	describe("index route", () => {
-		it("responds 200", (done) => {
+describe("get requests", function() {
+
+	this.retries(5);
+
+	describe("index route", function() {
+		it("responds 200", function(done) {
 			request(app).get("/")
 				.expect(200, done);
-		})
+		});
 	});
 
-	describe("articles route", () => {
-		it("responds 200", (done) => {
+	describe("articles base route", function() {
+		it("responds 200", function(done) {
 			request(app).get("/articles")
 				.expect(200, done);
-		})
-	})
+		});
+	});
+
+	describe("individual articles route", function() {
+		it("responds 200", function(done) {
+			request(app).get("/articles/1")
+				.expect(200, done);
+		});
+	});
 });
